b5f83cbb207509b4cf17de1c6a6fc5b26e96f16c
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-27 17:30:21 +0200

    setTitle function & addGenre function added

--- index.php
+++ index.php
@@ -1,28 +1,54 @@
 <?php
 
 class Movie{
-    public $title;
+
+    private $title;
     public $director;
     public $genre;
     public $runtime;
     public $tickets = true;
 
     function __construct($title, $director, $genre, $runtime){
-        $this->title = $title;
+
+        $this->setTitle($title);
         $this->director = $director;
-        $this->genre = $genre;
+        $this->genre = [$genre];
         $this->runtime = $runtime;
     }
     
+
+
     public function getTitle(){
         return $this->title;
     }
-    public function getDirector(){
-        return $this->direcor;
+
+    public function setTitle($title){
+        if(is_string($title)){
+            $this->title = $title;
+        }else{
+            $this->title = '';
+        }
     }
+
+    public function getDirector(){
+        return $this->director;
+    }
+
     public function getGenre(){
         return $this->genre;
     }
+    public function addGenre($newGenre){
+        if(is_string($newGenre) && !in_array($newGenre, $this->genre)){
+            $this->genre[] = $newGenre;
+        }
+    }
+
+    // public function removeGenre($genre){
+    //     if(in_array($genre)){
+
+    //     }
+    // }
+
     public function getRuntime(){
         return $this->runtime;
     }
@@ -32,10 +58,32 @@ class Movie{
 }
 
 $dune = new Movie('Dune', 'Denis Villeneuve', 'sci-fi', 155);
-
 $lotr_1 = new Movie('The Lord Of The Rings - The Fellwship of the Ring', 'Peter Jackson', 'fantasy', 178);
+$dune->addGenre('avventura');
 
-var_dump($dune);
-var_dump($dune->title);
+$movies = [$dune, $lotr_1];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php foreach($movies as $movie){  ?>
+
+        <div>
+            <h2><?= $movie->getTitle() ?></h2>
+            <h3><?= $movie->getDirector() ?></h3>
+            <h4><?php foreach($movie->getGenre() as $genre){ echo $genre.' '; }?> </h4>
+        </div>
+
+    <?php } ?>
+   
+</body>
+</html>
